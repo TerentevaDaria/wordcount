a01e6ede29ad4041e39fe3421214dcb4da3c1980
Your Name <you@example.com>
2022-10-06 20:30:31 +0300

    word counting

--- main.cpp
+++ main.cpp
@@ -3,6 +3,25 @@
 #include <fstream>
 #include <string>
 
+uint64_t WordCounter(const std::string& filename) {
+    std::ifstream file(filename);
+
+    char current_char;
+    char previous_char = ' ';
+    uint64_t word_count = 0;
+
+    for (current_char = file.get(); file.good(); current_char = file.get()) {
+        if (std::isspace(current_char) && !std::isspace(previous_char)) {
+            ++word_count;
+        }
+        previous_char = current_char;
+    }
+
+    file.close();
+
+    return word_count;
+}
+
 int main(int argc, char** argv) {
 
     std::vector <std::string> filenames;
@@ -20,6 +39,28 @@ int main(int argc, char** argv) {
         } else {
             filenames.push_back(arg);
         }
+    }
+
+    for (const std::string& filename : filenames) {
+        std::ifstream file(filename);
+
+        if (!file.is_open()) {
+            std::cout << "enable to open " << filename << '\n';
+            continue;
+        }
+
+        file.close();
+
+        if (words_arg) {
+            uint64_t word_count = WordCounter(filename);
+            std::cout << word_count << ' ';
+        }
+
+        if (!words_arg) {
+            uint64_t word_count = WordCounter(filename);
+            std::cout << word_count << ' ';
+        }
+        std::cout << filename << '\n';
     }
 
     return 0;
